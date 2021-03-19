0595f24fa3c64baf22c0ffe5c733b5281f0fa4ef
AdilRamdjan <20182716@student.hhs.nl>
2021-03-19 15:52:48 +0100

    Na gesprek met Bosch

--- src/Question.java
+++ src/Question.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Question{
     private String question;
     private Integer maxChoices;
     private ArrayList<String> choices;
     private Integer answer;
+    private Integer input;
 
     public void Question(String question, Integer maxChoices,  ArrayList<String> choices, Integer answer){
         this.question = question;
         this.maxChoices = maxChoices;
         this.choices = choices;
         this.answer = answer;
-
+        this.input = input;
     }
     public String getQuestion(){
         return question;
@@ -34,5 +36,23 @@ class Question{
     }
     public Boolean doQuestion(String[] randomChoice, Integer answer, Integer input){
 
+        System.out.println(this.getQuestion());
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextInt();
     }
+<<<<<<< Updated upstream
+=======
+
+    public String displayQuestion(String[] randomChoice){
+        String displayedQuestion = question;
+        for (int i = 0; i < this.maxChoices; i++){
+            displayedQuestion += "\n"+(i+1)+") " + randomChoice[i];
+        }
+        return displayedQuestion;
+    }
+
+    public Boolean validateInput(String input){
+
+    }
+>>>>>>> Stashed changes
 }
