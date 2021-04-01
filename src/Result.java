85f3e8f086f33fbf8bba985a166804a61d86db02
Frayen <kelvinjwz14@gmail.com>
2021-04-01 09:16:39 +0200

    add studentvalidation

--- src/Result.java
+++ src/Result.java
@@ -1,8 +1,29 @@
+import java.util.ArrayList;
+
 class Result{
-    Result hasStudent;
-    private Integer correct;
+    private Student hasStudent;
+    private ArrayList<ResultQuestion> hasResultQuestion;
+
+    public Result(Student hasStudent, ArrayList<ResultQuestion> hasResultQuestion) {
+        this.hasStudent = hasStudent;
+        this.hasResultQuestion = hasResultQuestion;
+    }
 
     public Boolean getResult(){
-        return true;
+        int correct = 0;
+        for (ResultQuestion resultQuestion: hasResultQuestion) {
+            if (resultQuestion.getResult()) {
+                correct++;
+            }
+        }
+        return (correct >= Math.round(hasResultQuestion.size() / 2));
+    }
+
+    public Student getHasStudent() {
+        return hasStudent;
+    }
+
+    public ArrayList<ResultQuestion> getHasResultQuestion() {
+        return hasResultQuestion;
     }
 }
